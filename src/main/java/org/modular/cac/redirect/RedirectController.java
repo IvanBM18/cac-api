e14f4d94ec1b55ece6278065df510e24909188ea
Ivan Barba <70910473+IvanBM18@users.noreply.github.com>
2024-08-24 01:19:29 -0600

    Redirecciona url a la UI de Swagger

---
+++ src/main/java/org/modular/cac/redirect/RedirectController.java
@@ -0,0 +1,20 @@
+package org.modular.cac.redirect;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/")
+public class RedirectController {
+
+    @Value("${springdoc.swagger-ui.path}")
+    String htmlLocation;
+
+    @GetMapping
+    public RedirectView redirectToSwaggerUi(){
+        return new RedirectView(htmlLocation);
+    }
+}
