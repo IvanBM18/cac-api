b996ef1e1b44aad459aef7cf967e3413f1ce0df0
Ivan Barba <70910473+IvanBM18@users.noreply.github.com>
2024-10-01 17:40:18 -0600

    Adds CORS configuration

---
+++ src/main/java/org/modular/cac/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package org.modular.cac.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*") //TODO:Cambiar en produccion
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
